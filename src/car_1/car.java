6c5fff28c2631c7570fd1eaeeb5952c225098924
MilaMicevska <micevska.mila@gmail.com>
2023-12-24 10:37:21 +0100

    p16

---
+++ src/car_1/car.java
@@ -0,0 +1,21 @@
+package car_1;
+
+public class car {
+	
+	private String make, model;
+	private year
+	Car() {
+		make="Unknow";
+		model="Unknow";
+		year=2023;
+		
+	}
+	Car (String m, String m1, int y)
+	{
+		this.make=m;
+		this.make=m1;
+		this.year=y;}
+	public String toString() {
+		return make + '\'+ model'+'\'+ year
+	}
+}
